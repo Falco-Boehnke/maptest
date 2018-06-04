3ab09588fb8f7a6fc513d854f4b2f19da3799e58
Shenanigator <falco.boehnke@gmail.com>
2018-06-04 23:42:47 +0200

    ergfdv

---
+++ android/src/com/mygdx/game/android/MyMapFragment.java
@@ -0,0 +1,21 @@
+package com.mygdx.game.android;
+
+import com.mygdx.game.SeMapInterface;
+
+/**
+ * Created by Falco Böhnke on 04.06.2018.
+ */
+
+public class MyMapFragment implements SeMapInterface {
+    public MyMapFragment(){
+
+    }
+
+    public void onCreate(){
+
+    }
+
+    public void onMapReady(){
+
+    }
+}
---
+++ core/src/com/mygdx/game/RequestHandler.java
@@ -0,0 +1,10 @@
+package com.mygdx.game;
+
+
+public interface RequestHandler {
+
+    public void addMap(final int x, final int y, final int width, final int height);
+
+    public void removeMap();
+
+}
